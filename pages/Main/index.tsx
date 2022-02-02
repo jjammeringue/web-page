6f861f211f8eb26d8642c210d312d4312e7b0a76
TaeSun94 <tyzlddy@naver.com>
2022-02-02 18:50:06 +0900

    Add: component flex design

--- pages/Main/index.tsx
+++ pages/Main/index.tsx
@@ -2,7 +2,15 @@
 export default function Main(){
 
     return(
-        <>
-        </>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+            <div style={{display: 'flex',flexDirection: 'row', justifyContent:'space-between'}}>
+                <div style={{display: 'flex',flexDirection: 'column'}}>
+                    dd
+                </div>
+                <div style={{display: 'flex',flexDirection: 'column'}}>
+                    kk
+                </div>
+            </div>
+        </div>
     )
 }
