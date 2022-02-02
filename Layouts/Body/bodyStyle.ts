f0ac23f9a351bc0330e52c53b188f6b03284118e
TaeSun94 <tyzlddy@naver.com>
2022-02-02 18:49:45 +0900

    Add: Basic Web fix

--- Layouts/Body/bodyStyle.ts
+++ Layouts/Body/bodyStyle.ts
@@ -1,8 +1,31 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-
-
+    padding-top: 40px;
 `;
 
-export default {Container};
+const rightBlock = styled.div`
+    position: absolute;
+    width: 22vw;
+    height: 34vw;
+    right:0px;
+    top: 0px;
+    background: rgba(193, 219, 252, 0.4);
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    font-size: xx-large;
+    writing-mode: vertical-rl;
+`;
+
+const leftBottomBlock = styled.div`
+    position: absolute;
+    width: 22vw;
+    height: 20vw;
+    left: 0px;
+    bottom: 0px;
+    background: rgba(193, 219, 252, 0.4);
+    writing-mode: vertical-lr;
+`;
+
+export default {Container, rightBlock, leftBottomBlock};
--- Layouts/Body/index.tsx
+++ Layouts/Body/index.tsx
@@ -1,12 +1,24 @@
 
 import Main from '../../pages/Main';
+import Styled from './bodyStyle';
+import {FaDiscord, FaMailBulk, FaPhoneAlt} from 'react-icons/fa'
 
 export default function Body(){
     return(
         <>
-        <div style={{position: 'absolute', width: '300px', height: '300px',left:'0px',bottom:'0px',background: 'rgba(193, 219, 252, 0.4)'}}/>
-        <div style={{position: 'absolute', width: '260px', height: '480px',right:'0px',top:'0px',background: 'rgba(193, 219, 252, 0.4)'}}/>
-        <Main />
+        <Styled.Container>
+            <Styled.leftBottomBlock>
+                <div>
+                    <FaDiscord size={'30'} style={{margin:'20px'}}/>
+                    <FaMailBulk size={'30'} style={{margin:'20px'}}/>
+                    <FaPhoneAlt size={'30'} style={{margin:'20px'}}/>
+                </div>
+            </Styled.leftBottomBlock>
+            <Styled.rightBlock>
+                01/01
+            </Styled.rightBlock>
+            <Main />
+        </Styled.Container>
         </>
     )
 }
