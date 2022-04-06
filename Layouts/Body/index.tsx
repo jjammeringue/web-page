128493baf6498be77adf2b566c37c1ad920f020d
TaeSun94 <tyzlddy@naver.com>
2022-04-06 21:38:33 +0900

    Del: 페이지 구성이 1개로 이뤄지므로 여러 Page로 나눌 필요가 없음

--- Layouts/Body/index.tsx
+++ Layouts/Body/index.tsx
@@ -1,23 +1,18 @@
-
 import Main from '../../pages/Main';
 import Styled from './bodyStyle';
-import {FaDiscord, FaMailBulk, FaPhoneAlt} from 'react-icons/fa'
 
 export default function Body(){
     return(
         <>
         <Styled.Container>
             <Styled.leftBottomBlock>
-                <div>
-                    <FaDiscord size={'30'} style={{margin:'20px'}}/>
-                    <FaMailBulk size={'30'} style={{margin:'20px'}}/>
-                    <FaPhoneAlt size={'30'} style={{margin:'20px'}}/>
-                </div>
             </Styled.leftBottomBlock>
             <Styled.rightBlock>
-                01/01
             </Styled.rightBlock>
-            <Main />
+            <Styled.InnerContiner>
+                
+            </Styled.InnerContiner>
+            {/* <Main /> */}
         </Styled.Container>
         </>
     )
